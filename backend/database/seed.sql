bd400ca28faa81de60e2b9a8d21ffae74b068592
Suzette <suzetteislam@gmail.com>
2020-04-03 18:05:22 -0400

    added additional tables

--- backend/database/seed.sql
+++ backend/database/seed.sql
@@ -7,8 +7,10 @@ CREATE TABLE users (
     firstname VARCHAR, 
     lastname VARCHAR, 
     email VARCHAR, 
-    password VARCHAR
-
+    password VARCHAR, 
+    user_avi VARCHAR, 
+    user_bio VARCHAR, 
+    history VARCHAR 
 );
 
 CREATE TABLE pantry (
@@ -27,21 +29,36 @@ CREATE TABLE shoppinglist (
 CREATE TABLE calendar (
     id SERIAL PRIMARY KEY, 
     user_id INT REFERENCES users(id), 
-    event VARCHAR, 
-    event_date DATE, 
-    event_description VARCHAR
+    event_date DATE 
 );
 
 CREATE TABLE recipes (
     id SERIAL PRIMARY KEY, 
-    name VARCHAR, 
+    recipe_name VARCHAR, 
     ingredients TEXT [], 
     directions VARCHAR
 );
  
--- CREATE TABLE pantry_party (
+CREATE TABLE groups (
+    id SERIAL PRIMARY KEY,
+    group_name VARCHAR,
+    group_members ARRAY 
+);
 
--- );
+CREATE TABLE event_group (
+    id SERIAL PRIMARY KEY, 
+    host_id INT REFERENCES users(id),
+    group_members 
+);
+
+CREATE TABLE pantry_party (
+    id SERIAL PRIMARY KEY, 
+    group INT REFERENCES event_group(id),
+    party_name VARCHAR, 
+    party_date REFERENCES calendar(date),
+    party_description VARCHAR,
+    recipe_info INT REFERENCES recipes(id)
+);
 
 
 INSERT INTO users (firstname, lastname, email, password)
