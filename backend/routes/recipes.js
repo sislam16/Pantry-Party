7cccb5fed0b519b2ebd7b4db23941b4904f4d51e
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-04-20 16:34:24 -0400

    Established and exported router, required express, getRecipeById, and allRecipesByUser

---
+++ backend/routes/recipes.js
@@ -0,0 +1,54 @@
+/*
+Recipes Route | Server | Tost-Host/Pantry Party Web App
+GROUP 7: Suzette Islam, Douglas MacKrell, Maliq Taylor
+*/
+
+var express = require('express');
+var router = express.Router();
+
+/* ROUTE HANDLES */
+
+// getRecipeById: get a single recipe by recipe ID.
+router.get("/api/recipe/:recipe_id", async (req, res, next) => {
+    try {
+        const recipeId = req.params.recipe_id
+        const recipe = await getRecipeById(recipeId);
+        console.log(recipe)
+        res.json({
+            status: "success",
+            message: `Recipe ${recipeId} retrieved`,
+            payload: recipe
+        });
+    } catch (err) {
+        res.json({
+            status: "failure",
+            message: "Oops! All Errors!!",
+            payload: null
+        })
+        throw err;
+    }
+});
+
+// allRecipesByUser: get all of a single user's recipes.
+router.get("/api/user/:user_id", async (req, res, next) => {
+    try {
+        const userId = req.params.user_id
+        const allRecipesByUser = await getAllRecipesByUserId(userId);
+        res.json({
+            status: "success",
+            message: `All recipes of user ${userId} retrieved`,
+            payload: allRecipesByUser
+        });
+    } catch (err) {
+        res.json({
+            status: "failure",
+            message: "Oops! All Errors!",
+            payload: null
+        })
+        throw err;
+    }
+});
+
+
+
+module.exports = router;
