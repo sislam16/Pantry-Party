d3fdcdd4a19e55c6fbc3fe6532f6b31c6f849528
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-04-20 16:38:12 -0400

    Queries database connection, GET getRecipeById, GET getAllRecipesById

--- backend/queries/recipes.js
+++ backend/queries/recipes.js
@@ -1,6 +1,28 @@
+/*
+Recipes Queries | Server | Tost-Host/Pantry Party Web App
+GROUP 7: Suzette Islam, Douglas MacKrell, Maliq Taylor
+*/
+
+// DATABASE CONNECTION
 const db = require('../database/db')
 
+//GET
 const getRecipeById = async (id) => {
     return await db.one('SELECT * FROM recipes WHERE id=$1', id)
+}
+
+//GET 
+const getAllRecipesByUserId = async (userId) => {
+    const getQuery = `SELECT * FROM recipes WHERE user_id = $1;`;
+    let recipes =  await db.any(getQuery, [userId]);
+    return recipes
+}
+
+
+
+/* EXPORT */
+module.exports = {
+    getRecipeById,
+    getAllRecipesByUserId
 }
 
