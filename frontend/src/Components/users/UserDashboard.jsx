7b901bde397b304a217f0167414c4d891be4c2ad
Suzette <suzetteislam@gmail.com>
2020-05-06 01:43:17 -0400

    created files for user dashboard, user public profile, user settings

---
+++ frontend/src/Components/users/UserDashboard.jsx
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Link, Route } from 'react-router-dom'
+
+const getRandomRecipeFromAPI = async () => {
+    try {
+    let randomRecipe = await axios.get('https://www.themealdb.com/api/json/v1/1/random.php')
+        console.log(randomRecipe)
+        this.setState({
+            recipeAPI: randomRecipe
+        })
+    } catch (error) {
+        console.log('err:', error)
+    }
+}
+const getCookbookRecipes = async () => {
+
+}
+
+const getEvents = async () => {
+
+}
+
+class UserDashboard extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            username: '',
+            recipeAPI: [],
+            cookbookRecipe: [],
+            events: []
+        }
+    }
+
+    render() {
+        return (
+            <div className='user-dashboard'>
+                <div className='dashboard-header'>
+
+                </div>
+
+                <div className='dashboard-live'>
+
+                </div>
+
+                <div className='dashboard-suggestions'>
+
+                </div>
+
+                <div className='dashboard-cookbook'>
+
+                </div>
+
+                <div className='dashboard-events'>
+
+                </div>
+            </div>
+        )
+    }
+}
+
+export default UserDashboard;
