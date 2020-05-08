e85c1e27c3041be9efb26deae608f1b66fc484f1
Suzette <suzetteislam@gmail.com>
2020-05-08 15:57:47 -0400

    refactored to hooks

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,63 @@
 
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import { Route, Switch } from "react-router-dom";
 import Error from "./components/Error";
-import Signup from "./components/Signup";
-import LogIn from "./components/LogIn";
 import AuthContainer from "./containers/AuthContainer";
-import Users from "./components/Users";
+import Users from "./components/users/Users";
+import UserDashboard from './components/users/UserDashboard'
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      userIdLoggedIn: 0,
-    };
-  }
+const App = ()=> {
 
+    const [username, setUsername] = useState("")
+    const [firstname, setFirstname] = useState("")
+    const [lastname, setLastname] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [isLoggedIn, setLoggedIn] = useState(false)
+    const [user, setUser] = useState(null)
 
-  render() {
-    const {
-      state: { userIdLoggedIn },
-    } = this;
 
     return (
       <div className="App">
         <Switch>
-          <Route exact path="/signup" component={AuthContainer} render={() => <Signup />} />
-          <Route exact path="/login" component={AuthContainer} render={() => <LogIn />} />
+          <Route exact path="/signup" >
+          <AuthContainer
+          username={username} 
+          firstname = {firstname} 
+          lastname = {lastname} 
+          password = {password} 
+          email ={email} 
+          user={user}
+          isLoggedIn={isLoggedIn}
+          setUsername = {setUsername}
+          setFirstname = {setFirstname}
+          setLastname = {setLastname}
+          setEmail = {setEmail}
+          setPassword = {setPassword}
+          setLoggedIn = {setLoggedIn}
+          setUser = {setUser}
+          />
+          </Route>
+          
+          <Route exact path="/login"><AuthContainer 
+          username = {username} 
+          password={password}
+          user={user} 
+          isLoggedIn={isLoggedIn}
+          setUsername = {setUsername}
+          setPassword = {setPassword}
+          setLoggedIn = {setLoggedIn}
+          setUser={setUser}
+          />
+          </Route>
+          <Route exact path ='/home' component = {UserDashboard} />
           <Route exact path = '/users' component = {Users} />
+
           <Route path="*" render={() => <Error />} />
         </Switch>
       </div>
     );
   }
-}
+
 
 export default App;
