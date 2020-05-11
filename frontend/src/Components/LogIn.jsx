9e50f53b7f60d22bc5531b4150440b554c9bb0ac
Suzette <suzetteislam@gmail.com>
2020-05-11 15:01:07 -0400

    removed warning info

--- frontend/src/Components/LogIn.jsx
+++ frontend/src/Components/LogIn.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { Link, Redirect} from "react-router-dom";
 
 const Landing =({username, password, setUsername, setPassword, isLoggedIn, setLoggedIn, loginUser}) => {
