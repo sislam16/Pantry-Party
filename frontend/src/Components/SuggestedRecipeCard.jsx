45dca8c47d4b52a305b4e52e9e4f4d9a72232a6e
Suzette <suzetteislam@gmail.com>
2020-05-11 14:56:48 -0400

    created suggested recipe card from the meal db api

--- frontend/src/Components/SuggestedRecipeCard.jsx
+++ frontend/src/Components/SuggestedRecipeCard.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 
 const SuggestedRecipeCard = (props) => {
     return(
-        <div className='api-recipe'>
-            <h1></h1>
-            
+        <div className='api-recipe' > 
+        {/*  make a function that expands thumbnail to recipe page */ }
+            <img src ={props.strMealThumb} />
+            <h1>{props.strMeal}</h1>
         </div>
     )
 }
