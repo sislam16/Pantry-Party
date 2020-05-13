1546f4bb024a1abd80a9cda89aee17a9f6a00470
Suzette <suzetteislam@gmail.com>
2020-05-13 01:50:22 -0400

    created folders for files pertaining to recipes

---
+++ frontend/src/Components/recipes/NewRecipe.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const NewRecipe = () => {
+    return(
+        <div>
+            <form action="">
+                <input type="text" placeholder='name'/> 
+            </form>
+        </div>
+    )
+
+}
+
+export default NewRecipe
