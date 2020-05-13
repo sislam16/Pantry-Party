700d00547eb7f5f377725247a55b853e770b42be
Suzette <suzetteislam@gmail.com>
2020-05-13 01:52:32 -0400

    can popular user info into the header and established area for history/past recipes streamed

--- frontend/src/Components/users/UserPublic.jsx
+++ frontend/src/Components/users/UserPublic.jsx
@@ -12,7 +12,10 @@ const UserPublic = ({ user }) => {
                 <h2 className='username'>{user.username}</h2>
                 <p className='user-bio'><span><strong>About Me:</strong></span>{user.bio}</p>
             </div>
-
+            <div className='userHistory'>
+            <h3>HISTORY</h3>
+            {/* past events/recipes are supposed to show up here */}
+            </div>
         </div>
     )
 }
