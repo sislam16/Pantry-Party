27b8609200fd123a823e6635d9e64657aeb75fa0
Suzette <suzetteislam@gmail.com>
2020-05-17 12:43:29 -0400

    frontend fixes

---
+++ frontend/src/Components/PrivateRoute.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Route} from 'react-router-dom'
+
+const PrivateRoute = ({isUserLoggedIn, component}) =>{
+return(
+    <div>
+
+    </div>
+)
+}
+
+export default PrivateRoute
