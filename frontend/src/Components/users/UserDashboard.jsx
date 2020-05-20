9cc7cc464be3376ce3a7fa3b6a607d9ae27d65e7
Suzette <suzetteislam@gmail.com>
2020-05-19 20:55:01 -0400

    import events and API recipe components

--- frontend/src/Components/users/UserDashboard.jsx
+++ frontend/src/Components/users/UserDashboard.jsx
@@ -1,92 +1,27 @@
-import React, { useEffect, useState, useLayoutEffect, useRef } from 'react';
-import axios from 'axios';
-import { Link, Route } from 'react-router-dom'
-import SuggestedRecipeCard from '../SuggestedRecipeCard'
+import React, { useEffect, useState, useLayoutEffect, useRef } from 'react';
+import axios from 'axios';
+import { Link, Route } from 'react-router-dom'
+import SuggestedRecipeCard from '../recipes/SuggestedRecipeCard'
+import ApiRecipesComponent from '../recipes/ApiRecipeComponent';
+import EventComponentUD from '../events/EventsComponent'
 
 
-const UserDashboard = ({ user }) => {
-     console.log(user)
+const UserDashboard = ({ user }) => {
+    console.log(user)
 
-    const [recipeArr, setRecipeArr] = useState([]) 
-    const [cbrecipeArr, setCbRecipeArr] = useState([])
-    const [eventsArr, setEventsArr] = useState([])
+    return (
+        <div className='user-dashboard'>
+            <h1>Welcome {user.username}</h1>
+            <ApiRecipesComponent />
 
-    // similar to component did mount
-    useEffect(() => {
-        const populateRecipeArr = async () => {
-            let r1 = getRandomRecipeFromAPI()
-            let r2 = getRandomRecipeFromAPI()
-            let r3 = getRandomRecipeFromAPI()
-            let r4 = getRandomRecipeFromAPI()
+            <div className='dashboard-cookbook'>
 
-            let recipes = await Promise.all([r1, r2, r3, r4]) 
-            console.log('hello recipes',recipes) 
-            setRecipeArr(recipes)
-            // console.log('arr:', recipeArr)
-        }
-             populateRecipeArr();
-        
-        // getEvents();
- }, [])
+            </div>
 
-    //gets random recipe from API
-    const getRandomRecipeFromAPI = async () => {
-        try {
-            let { data } = await axios.get('https://www.themealdb.com/api/json/v1/1/random.php')
-            let recipe = data.meals[0]
-            console.log("heree", recipe)
-            return recipe
-        } catch (error) {
-            console.log('err:', error)
-        }
-    }
-
-    // const getEvents = async () => {
-    //     let user_id = user.id
-    //     // console.log(user_id)
-    //     try {
-    //         let loggedinUserEvents = await axios.get(`/events/user/${user_id}`)
-    //         console.log(loggedinUserEvents)
-    //     } catch (error) {
-    //         console.log(error)
-    //     }
-    // }
-
-const suggestedRecipeThumbnail = recipeArr.map(el => 
-    <SuggestedRecipeCard
-         id={el.idMeal}
-         imgSrc={el.strMealThumb}
-         recipeName={el.strMeal}
-         alt='db recipe'
-     >
-         {el}
-    </SuggestedRecipeCard >)
-
-    console.log(suggestedRecipeThumbnail)
-    console.log(recipeArr)
-
-
-
-    return (
-        <div className='user-dashboard'>
-            <div className='dashboard-header'>
-                <h1>hey {user.username}</h1>
-            </div>
-
-            <div className='dashboard-suggestions'>
-                {suggestedRecipeThumbnail}
-            </div>
-
-            <div className='dashboard-cookbook'>
-
-            </div>
-
-            <div className='dashboard-events'>
-
-            </div>
-        </div>
-    )
+            <EventComponentUD />
+        </div>
+    )
 }
 
 
-export default UserDashboard;
+export default UserDashboard;
