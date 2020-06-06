364005dc85245aac57da02ea42f4fe1ade4de4fa
Suzette <suzetteislam@gmail.com>
2020-06-05 22:00:50 -0400

    began the new recipe form

--- frontend/src/Components/recipes/NewRecipe.jsx
+++ frontend/src/Components/recipes/NewRecipe.jsx
@@ -1,12 +1,33 @@
-import React from 'react'
-
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const NewRecipe = () => {
-    return(
+    const [recipe_name, setRecipeName] = useState('')
+    const [ingredients, setIngredients] = useState('')
+    const [directions, setDirections] = useState('')
+
+    const handleSubmit = async (e) =>{
+        e.preventDefault()
+
+        let bodyObj = {
+            recipe_name: recipe_name,  
+            directions: directions, 
+        }
+
+        try{
+        let {data} = await axios.post(`/api/recipes/new`, bodyObj)
+        console.log(data.payload)
+        } catch(error) {
+            console.log('error:', error)
+        }
+    }
+    return (
         <div>
-            <form action="">
-                <input type="text" placeholder='name'/> 
-                 <input type="text" placeholder ='ingredients'/>
+            <form action="" className = 'newRecipeForm' onSubmit={handleSubmit}>
+                <input type="text" placeholder='name' onChange={e => setRecipeName(e.target.value)} />
+                <input type="text" placeholder='ingredients' onChange={e => setIngredients(e.target.value)} />
+                <input type="text" placeholder='directions' onChange={e => setDirections(e.target.value)} />
+                <button>Submit</button>
             </form>
         </div>
     )
