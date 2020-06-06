ffce808a815052ce73bf3f405e2efb6e4a75086e
Suzette <suzetteislam@gmail.com>
2020-06-05 22:03:51 -0400

    created cookbook  card for user dashboard

--- frontend/src/Components/recipes/CookbookCard.jsx
+++ frontend/src/Components/recipes/CookbookCard.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 
-const CookbookRecipeCard = ({recipeName, imgSrc, id}) => {
+const CookbookRecipeCard = ({recipe_name, recipe_img, id}) => {
+    console.log('name:', recipe_name)
+    console.log('img link:', recipe_img)
+    
     return(
+        <Link to ={`/cookbook/recipe/${id}`}>
         <div className='api-recipe' id={id} > 
         {/*  make a function that expands thumbnail to recipe page */ }
-            <img src ={imgSrc} />
-            <h1>{recipeName}</h1>
+            <img src ={recipe_img} />
+            <h1>{recipe_name}</h1>
         </div>
+        </Link>
     )
 }
 export default CookbookRecipeCard
