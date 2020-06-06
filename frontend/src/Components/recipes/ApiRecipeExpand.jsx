706e6485f00addd7a67aca866e9a1f0e1fb79d37
Suzette <suzetteislam@gmail.com>
2020-06-05 22:01:36 -0400

    modified file, still need to add react youtube

--- frontend/src/Components/recipes/ApiRecipeExpand.jsx
+++ frontend/src/Components/recipes/ApiRecipeExpand.jsx
@@ -2,16 +2,14 @@ import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import axios from 'axios'
 
-
-
 const APIRecipe = ({ user }) => {
     const [apiRecipe, setApiRecipe] = useState({})
+    const [ingredientArr, setIngredientArr] = useState([])
+    const [measureArr, setMeasureArr] = useState([])
     const { recipe_id } = useParams()
-    console.log(recipe_id)
 
     useEffect(() => {
         const getFullRecipeById = async () => {
-            // let id = match.id
             try {
                 let { data } = await axios.get(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${recipe_id}`)
                 let recipe = data.meals[0]
@@ -20,14 +18,30 @@ const APIRecipe = ({ user }) => {
             } catch (error) {
                 console.log('error:', error)
             }
-
         }
-
         getFullRecipeById()
     }, [])
 
-
-
+    useEffect(() =>{
+        const apiIngredientsList = () => {
+            let ingList = []
+            let measureList = []
+            for (let key in apiRecipe) {
+                if (key.includes('strIngredient')) {
+                    ingList.push(apiRecipe[key])
+                    setIngredientArr(ingList)
+                }
+                if (key.include('strMeasure')) {
+                    measureList.push(apiRecipe[key].value)
+                    setMeasureArr(measureList)
+                }
+            }
+            console.log(ingList)
+            console.log(measureList)
+        }
+        apiIngredientsList()
+    },[])
+  
     return (
         <div className="meal-db-recipe">
             <h1>{apiRecipe.strMeal}</h1>
