aa06153ade300b642b5ece0e57d8c387725fff5c
Suzette <suzetteislam@gmail.com>
2020-06-05 21:56:29 -0400

    created New Event form where user creates new event for stream

--- frontend/src/Components/events/NewEvent.jsx
+++ frontend/src/Components/events/NewEvent.jsx
@@ -1,19 +1,52 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import EventComponentUD from './EventsComponent'
 
+const NewEvent = ({user}) => {
+    // const [eventsArr, setEventsArr] = useState([])
+    const [event_name, setEventName] = useState('')
+    const [event_date, setEventDate] = useState('')
+    const [event_description, setEventDescription] = useState('')
+    const [recipe_id, setRecipeId] = useState('')
+    const [newEvent, setNewEvent] = useState({})
 
-const NewEvent = () =>{
-return(
-    <div>
-        <form action="" className='newEvent-form'>
-            <label htmlFor="eventName">Event Name:</label>
-            <input type="text"/>
-            <label htmlFor="eventDate">eventDate</label>
-            <input type ="date"/>
-            <button>Create</button>
-        </form>
-    </div>
-)
+    const createNewEvent = async (e) => {
+        e.preventDefault();
+       
+        try {
+            let bodyObj = {
+                event_name: event_name,
+                event_date: event_date,
+                event_description: event_description,
+                recipe_id: recipe_id
+            }
+            let response  = await axios.post(`/api/events/new`, bodyObj)
+            let event = response.data.payload
+            setNewEvent(event)
+        } catch (error) {
+            console.log('error:', error)
+        }
+    }
+
+    return (
+        <div>
+            <form action="" className='newEvent-form' onSubmit={createNewEvent}>
+                <label htmlFor="eventName">Event Name:</label>
+                <input type="text" name='event_name' onChange={(e) => setEventName(e.target.value)} />
+                <br>
+                </br>
+                <label htmlFor="eventDate">Event Date:</label>
+                <input type="date" name='event_date' onChange={(e) => setEventDate(e.target.value)} />
+                <br />
+                <label htmlFor='eventDescription'>Event Description:</label>
+                <input type="text" name='event_description' onChange={(e) => setEventDescription(e.target.value)} />
+                <br />
+                <label htmlFor='recipe-Info'>Recipe Info:</label>
+                <input type="number" name='recipe_id' onChange={(e) => setRecipeId(e.target.value)} />
+                <button>Create</button>
+            </form>
+        </div>
+    )
 }
 
 export default NewEvent
