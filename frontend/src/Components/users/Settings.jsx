c087557389d292bd15fe8e96ebd98d5de73de026
Suzette <suzetteislam@gmail.com>
2020-06-08 18:48:39 -0400

    import material-ui, modified values in input boxes through the state

--- frontend/src/Components/users/Settings.jsx
+++ frontend/src/Components/users/Settings.jsx
@@ -1,22 +1,114 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios';
+import { TextField, Button, Typography } from '@material-ui/core'
 
+const Settings = ({ user }) => {
+    const [edit, setEdit] = useState(false)
+    const [username, setUsername] = useState(user.username)
+    const [firstname, setFirstname] = useState(user.firstname)
+    const [lastname, setLastname] = useState(user.lastname)
+    const [bio, setBio] = useState(user.bio)
 
+    const handleUpdate = async () => {
 
+        try {
+            let updateInfo = await axios.post('')
+        } catch (error) {
+            console.log('error:', error)
+        }
+    }
+    const handleEdit = (e) => {
+        e.preventDefault()
+        console.log('editing stuff')
+        setEdit(true)
+    }
 
-const Settings = ({user}) =>{
-return(
-    <div>
-        <h1>Settings</h1>
-        <form>
-          <h4>username:</h4>  <p>{user.username}</p>
-          <h4>firstname:</h4>  <p>{user.firstname}</p>
-          <h4>lastname:</h4>  <p>{user.lastname}</p>
-          <h4>Bio:</h4> <p>{user.bio}</p>
-           <button>Edit</button> <button>Deactivate Account</button>
+    const deactivateAccount = (e) => {
+        e.preventDefault()
+        console.log('deactivating account')
+        try {
 
-        </form>
-    </div>
-)
+        } catch (error) {
+            console.log('error:', error)
+        }
+    }
+
+    // const handleChange = (e) => {
+    //     e.preventDefault()
+    //     console.log(e.target.value)
+    // }
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log('submitting updated info')
+        try {
+
+        } catch (error) {
+            console.log('error', error)
+        }
+    }
+
+    if (edit === false) {
+        return (
+            <div>
+                <Typography>Settings</Typography>
+                <form>
+                    <label htmlFor="username">username:</label> <p>{user.username}</p>
+                    <label htmlFor="firstname">firstname:</label> <p>{user.firstname}</p>
+                    <label htmlFor="lastname">lastname:</label> <p>{user.lastname}</p>
+                    <label htmlFor="bio">Bio:</label> <p>{user.bio}</p>
+                    <Button onClick={handleEdit}>Edit</Button> <Button onClick={deactivateAccount}>Deactivate Account</Button>
+                </form>
+            </div>
+        )
+    } else {
+        console.log('username', username)
+        return (
+            <div>
+                <Typography
+                variant='h2'
+                >Settings</Typography>
+                <form>
+                    <TextField
+                        variant="filled"
+                        margin='normal'
+                        label="Username"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}>
+                        {username}
+                    </TextField><br />
+
+                    <TextField
+                        variant="filled"
+                        margin='normal'
+                        label='First Name'
+                        value={firstname}
+                        onChange={(e) => setFirstname(e.target.value)}>
+                        {firstname}
+                    </TextField> <br />
+
+                    <TextField
+                        variant="filled"
+                        margin='normal'
+                        label='Last Name'
+                        value={lastname}
+                        onChange={(e) => setLastname(e.target.value)}>
+                        {lastname}
+                    </TextField><br />
+
+                    <TextField
+                        variant="filled"
+                        margin='normal'
+                        label="Bio"
+                        value={bio}
+                        onChange={(e)=>setBio(e.target.value)}>
+                        {bio}
+                    </TextField> <br />
+
+                    <Button onClick={handleSubmit}>Submit</Button>
+                </form>
+            </div>
+        )
+    }
 }
 
 export default Settings
