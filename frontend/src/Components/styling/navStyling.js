b808e8ba054473aa565ed5adec0140d780140ea0
Suzette <suzetteislam@gmail.com>
2020-06-07 21:42:21 -0400

    created js styling files for material-ui

---
+++ frontend/src/Components/styling/navStyling.js
@@ -0,0 +1,18 @@
+import {makeStyles} from '@material-ui/core'
+import customTheme from './customTheme'
+
+
+export const navStyles = makeStyles((theme) =>({
+    root: {
+        flexGrow: 1,
+      },
+      menuButton: {
+        marginRight: theme.spacing(2),
+      },
+      title: {
+        flexGrow: 1,
+      }, 
+      appBar:{
+        backgroundColor: customTheme.palette.secondary.main
+      },
+}))
