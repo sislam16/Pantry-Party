9a2fae35a3785fbfbc6d63a23bbf30606a838ab4
Suzette <suzetteislam@gmail.com>
2020-06-08 18:47:33 -0400

    imported nav bar from material ui with icons

--- frontend/src/Components/Nav.jsx
+++ frontend/src/Components/Nav.jsx
@@ -1,24 +1,81 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
+import { AppBar, Toolbar, IconButton, Typography, Button } from '@material-ui/core'
+import { navStyles } from './styling/navStyling';
+import HomeIcon from '@material-ui/icons/Home';
+import PersonIcon from '@material-ui/icons/Person';
+import SettingsIcon from '@material-ui/icons/Settings';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import LiveTvIcon from '@material-ui/icons/LiveTv';
 
-const NavBar = ({logoutUser, isLoggedIn}) => {
-    if(isLoggedIn){
-    return (
-        <nav>
-            <Link to ='/home'>Home</Link>
-            <Link to='/profile'>Profile</Link>
-            <Link to ='/settings'>Settings</Link>
-            <button onClick= {logoutUser}>Log-out</button>
-        </nav>
-    )
-    } 
-    return(
-        <nav>
-            <Link to= '/signup'>Sign Up</Link>
-            <Link to= '/login'>Log In</Link>
-        </nav>
-    )
-    
+const NavBar = ({ logoutUser, isLoggedIn }) => {
+    const classes = navStyles()
+    const history = useHistory()
+
+    const rerouteNav = (route) => {
+        history.push(`/${route}`)
+    }
+
+    if (isLoggedIn) {
+        return (
+            <AppBar position="static" className={classes.appBar}>
+                <Toolbar>
+                <Typography edge='start'variant="h6" className={classes.title}>
+                        Pantry Party
+                    </Typography>
+
+                    <IconButton
+                        edge="start"
+                        className={classes.menuButton}
+                        color="inherit"
+                        aria-label="menu"
+                        onClick={() => { rerouteNav('home') }}
+                    >
+                        <HomeIcon />
+                    </IconButton>
+
+                    <IconButton
+                        edge="start"
+                        className={classes.menuButton}
+                        color="inherit"
+                        aria-label="menu"
+                        onClick={() => { rerouteNav('profile') }}
+                    >
+                        <PersonIcon />
+                    </IconButton>
+
+                    <IconButton
+                        edge="start"
+                        className={classes.menuButton}
+                        color="inherit"
+                        aria-label="menu"
+                        onClick={() => { rerouteNav('settings') }}
+                    >
+                        <SettingsIcon />
+                    </IconButton>
+
+                  <IconButton
+                  edge='start'
+                  color='inherit'
+                  aria-label='menu'
+                  onClick={()=>{rerouteNav('/live')}}>
+                      <LiveTvIcon />
+                  </IconButton>
+
+                    <IconButton
+                        edge="start"
+                        className={classes.menuButton}
+                        color="inherit"
+                        aria-label="menu"
+                        onClick={logoutUser}>
+                        <ExitToAppIcon />
+                    </IconButton>
+                </Toolbar>
+
+            </AppBar>
+        )
+    }
+    return null
 }
 
 export default NavBar;
