42b477ea9e42828063baa69fd77d70b06230eab3
Suzette <suzetteislam@gmail.com>
2020-06-08 19:24:14 -0400

    import material-ui

--- frontend/src/Components/recipes/CookbookCard.jsx
+++ frontend/src/Components/recipes/CookbookCard.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import Card from '@material-ui/core/Card';
 
-const CookbookRecipeCard = ({recipe_name, recipe_img, id}) => {
+const CookbookRecipeCard = ({ recipe_name, recipe_img, id }) => {
     console.log('name:', recipe_name)
     console.log('img link:', recipe_img)
-    
-    return(
-        <Link to ={`/cookbook/recipe/${id}`}>
-        <div className='api-recipe' id={id} > 
-        {/*  make a function that expands thumbnail to recipe page */ }
-            <img src ={recipe_img} />
-            <h1>{recipe_name}</h1>
-        </div>
+
+    return (
+        <Link to={`/cookbook/recipe/${id}`}>
+            <Card className='api-recipe' id={id} variant='outlined'>
+                <h1>{recipe_name}</h1>
+                <img src={recipe_img} className='api-img' />
+            </Card>
         </Link>
     )
 }
