7d179896024262910b7ceafa83f5d7eed232a09c
Suzette <suzetteislam@gmail.com>
2020-06-08 18:52:12 -0400

    import material-ui components

--- frontend/src/Components/recipes/SuggestedRecipeCard.jsx
+++ frontend/src/Components/recipes/SuggestedRecipeCard.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import {Card, Typography} from '@material-ui/core';
 
 const SuggestedRecipeCard = ({recipeName, imgSrc, id}) => {
     console.log(recipeName, imgSrc)
     return(
         <Link to={`/recipe/random/${id}`}>
-        <div className='api-recipe' id={id} > 
-        {/*  make a function that expands thumbnail to recipe page */ }
-            <img src ={imgSrc} />
-            <h1>{recipeName}</h1>
-        </div>
+        <Card className='api-recipe' id={id} > 
+        <div className = 'api-title'><Typography variant='h5'>{recipeName}</Typography></div>
+            <div className = 'api-img'><img src ={imgSrc} /></div>
+            
+            
+        </Card>
         </Link>
     )
 }
