f856bec091015d8c9ca40c2025593ff5e0b30f95
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-06-08 22:44:04 -0400

    LinksContainer and useUserMedia added

---
+++ frontend/src/Components/useUserMedia.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react"
+
+const useUserMedia = requestedMedia => {
+  const [mediaStream, setMediaStream] = useState(null);
+
+  // initialize streams
+  useEffect(() => {
+    const enableStream = async () => {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia(requestedMedia);
+        setMediaStream(stream);
+      } catch(err) {
+        console.log(err)
+      }
+    }
+
+    if (!mediaStream) {
+      enableStream();
+    } else {
+      return function cleanup() {
+        mediaStream.getTracks().forEach(track => {
+          track.stop();
+        });
+      }
+    }
+  }, [mediaStream, requestedMedia]);
+
+  return mediaStream;
+}
+
+export default useUserMedia
