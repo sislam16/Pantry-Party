b8b84ba59e5d1c99fc9dd2c71630d6077a57b332
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-06-10 04:02:39 -0400

    display links updated with usernames

--- frontend/src/Components/broadcast/BroadcasterLink.jsx
+++ frontend/src/Components/broadcast/BroadcasterLink.jsx
@@ -1,13 +1,23 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 const BroadcasterLink = ({ broadcaster }) => {
+    const [username, setUserName] = useState('')
     let broadcasterId = broadcaster.broadcast_id
     let name = broadcaster.event_name
     let description = broadcaster.event_description
 
+    const getUsername = async () => {
+        let displayName = await (await axios.get(`api/users/id/${broadcaster.user_id}`)).data.payload.username
+        setUserName(displayName)
+        console.log("username", displayName)
+    }
+    useEffect(() => {
+        getUsername()
+    }, [])
 
-    return <li><Link to={`/watch/${broadcasterId}`}>{name}: {description}</Link></li>
+    return <li><Link to={`/watch/${broadcasterId}`}>{name}: {description} | HOST: {username}</Link></li>
 }
 
 export default BroadcasterLink
