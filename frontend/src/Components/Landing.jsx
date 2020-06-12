b1318ad45aa67cff7b958c8fc5ee8ec3a5e44c3e
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-06-11 20:42:22 -0400

    It's party time

--- frontend/src/Components/Landing.jsx
+++ frontend/src/Components/Landing.jsx
@@ -3,6 +3,8 @@ import { Button, makeStyles } from '@material-ui/core';
 import { useHistory } from 'react-router-dom';
 import { authStyles } from './styling/AuthStyling'
 import logo from './additional/PantryPartyLogo.svg'
+import useWindowSize from 'react-use/lib/useWindowSize'
+import Confetti from 'react-confetti'
 
 
 const Landing = () => {
@@ -12,12 +14,18 @@ const Landing = () => {
         history.push(`/${route}`)
     }
 
+    const { width, height } = useWindowSize()
+
     return (
         <div className='landing'>
             <img src={logo} alt='logo' /> <br />
             <Button className={classes.button} variant='outlined' size='large' onClick={() => { rerouteAuth('login') }}>
                 Login</Button>
             <Button className={classes.button} variant='outlined' size='large' onClick={() => { rerouteAuth('signup') }}>Sign up</Button>
+            <Confetti
+                width={width}
+                height={height}
+            />
         </div>
     )
 }
