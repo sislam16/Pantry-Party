20e75feb2bbafb5e7aba7a12e5ba61dfd6626106
Suzette <suzetteislam@gmail.com>
2020-06-11 21:00:20 -0400

    styling of new recipe form and new event

--- frontend/src/Components/recipes/NewRecipe.jsx
+++ frontend/src/Components/recipes/NewRecipe.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import {useHistory} from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
 import axios from 'axios';
-import { TextField, Button, CheckboBx, Typography } from '@material-ui/core'
+import { TextField, Button, Typography, Container } from '@material-ui/core'
 import { recipeStyles } from '../styling/recipesStyling'
 
 const NewRecipe = () => {
@@ -20,7 +20,7 @@ const NewRecipe = () => {
             recipe_name: recipe_name,
             recipe_img: recipe_img,
             directions: directions,
-            ingredients: ingredients, 
+            ingredients: ingredients,
             hashtags: hashtags.split(',')
         }
 
@@ -28,23 +28,23 @@ const NewRecipe = () => {
             console.log('posting')
             let { data } = await axios.post(`/api/recipes/new`, bodyObj)
             console.log('recipe', data.payload)
-            history.push('/home') //redirect to cookbook when component is made 
+            history.push('/cookbook') //redirect to cookbook when component is made 
         } catch (error) {
             console.log('error:', error)
         }
     }
 
     const addIngredientRow = (e) => {
-        const copy = [...ingredients]
-        copy.push({ ingredient: '', amount: '', measure: '' }) 
-        setIngredients(copy)
+        const copy = [...ingredients] //copy of arr in state
+        copy.push({ ingredient: '', amount: '', measure: '' }) //push in a new obj for new ing
+        setIngredients(copy) //set the state again 
     }
 
     const handleChange = (e, index, key) => {
         console.log(key)
-        const copy =[...ingredients]
-        copy[index][key] = e.target.value
-        setIngredients(copy)
+        const copy = [...ingredients] //copy arr in state
+        copy[index][key] = e.target.value // assign its value as input fields are being changed 
+        setIngredients(copy) // set the state with ingredients filled out 
     }
 
     const inputFields = ingredients.map((ing, index) =>
@@ -57,21 +57,20 @@ const NewRecipe = () => {
 
 
     return (
-        <div>
-            <Typography variant='h4'>Add Recipe to Cookbook </Typography>
-
-            <form className='newRecipe'>
-                <TextField variant='filled' id="standard-size-small" type="text" value={recipe_name} label='Recipe Name' onChange={e => setRecipeName(e.target.value)} margin='normal' /> <br />
-                <TextField variant='filled' id="standard-size-small" type='text' value={recipe_img} label='recipe_img' onChange={e => setRecipeImg(e.target.value)} margin='normal' /><br />
-                <TextField variant='filled' id="standard-size-small" type="text" value={directions} label='Directions' onChange={e => setDirections(e.target.value)} margin='normal' helperText="Separate steps by ','" /><br />
-                <div>
-                    {inputFields}
-                    <span onClick={addIngredientRow}>+</span><br />
-                </div>
-                <TextField variant='filled' id="standard-size-small" type='text' label='Hashtags' margin='normal' helperText="Separate hashtags by ','"  value = {hashtags} onChange = {e => setHashtags(e.target.value)}/> <br />
-                <Button onClick={handleSubmit}>Submit</Button>
-            </form>
-        </div>
+            <Container fixed style={{height: '70vh', backgroundColor: '#ffffff', width: '50vw', marginTop:'20px'}}>
+                <form className='newRecipe'>
+                    <Typography variant='h3'style={{color:'#ed7902', fontWeight:'bold'}}>Create Recipe </Typography>
+                    <TextField variant='filled' id="standard-size-small" type="text" value={recipe_name} label='Recipe Name' onChange={e => setRecipeName(e.target.value)} margin='normal' /> <br />
+                    <TextField variant='filled' id="standard-size-small" type='text' value={recipe_img} label='recipe_img' onChange={e => setRecipeImg(e.target.value)} margin='normal' /><br />
+                    <TextField variant='filled' id="standard-size-small" type="text" value={directions} label='Directions' onChange={e => setDirections(e.target.value)} margin='normal' helperText="Separate steps by ','" /><br />
+                    <div>
+                        {inputFields}
+                        <span onClick={addIngredientRow}>+</span><br />
+                    </div>
+                    <TextField variant='filled' id="standard-size-small" type='text' label='Hashtags' margin='normal' helperText="Separate hashtags by ','" value={hashtags} onChange={e => setHashtags(e.target.value)} /> <br />
+                    <Button onClick={handleSubmit}>Submit</Button>
+                </form>
+            </Container>
     )
 
 }
