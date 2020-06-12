36b7c78160f05755e3753cb9b856d64f43527972
Suzette <suzetteislam@gmail.com>
2020-06-11 20:58:46 -0400

    user public styling

--- frontend/src/Components/users/UserPublic.jsx
+++ frontend/src/Components/users/UserPublic.jsx
@@ -1,20 +1,21 @@
 import React from 'react'
 import axios from 'axios';
+import {Container, Typography} from '@material-ui/core'
 
 const UserPublic = ({ user }) => {
 
     return (
-        <div className='user-public'>
+       <Container style={{marginTop:'20px'}}>
             <div className='userInfo'>
                 <img src={user.avatar} className='user-avatar' />
-                <h2 className='username'>{user.username}</h2>
+                <Typography variant ='h3' style={{fontWeight: 'bold', color:'#ed7902'}}>{user.username}</Typography>
                 <p className='user-bio'><span><strong>About Me:</strong></span>{user.bio}</p>
             </div>
             <div className='userHistory'>
             <h3>HISTORY</h3>
             {/* past events/recipes are supposed to show up here */}
             </div>
-        </div>
+            </Container>
     )
 }
 
