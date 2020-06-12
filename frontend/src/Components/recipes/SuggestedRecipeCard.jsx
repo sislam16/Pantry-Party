0e801c1bdea4a19e2b324903c14fca575cb3ee3c
Suzette <suzetteislam@gmail.com>
2020-06-11 21:08:39 -0400

    modified styling of suggested recipe component on user dash and cards for recipes

--- frontend/src/Components/recipes/SuggestedRecipeCard.jsx
+++ frontend/src/Components/recipes/SuggestedRecipeCard.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
-import {Card, Typography} from '@material-ui/core';
+import { Link } from 'react-router-dom'
+import { Card, Typography } from '@material-ui/core';
 
-const SuggestedRecipeCard = ({recipeName, imgSrc, id}) => {
-    console.log(recipeName, imgSrc)
-    return(
-        <Link to={`/recipe/random/${id}`}>
-        <Card className='api-recipe' id={id} > 
-        <div className = 'api-title'><Typography variant='h5'>{recipeName}</Typography></div>
-            <div className = 'api-img'><img src ={imgSrc} /></div>
-        </Card>
+const SuggestedRecipeCard = ({ recipeName, imgSrc, id }) => {
+    return (
+        <Link to={`/recipe/random/${id}`} style={{ textDecoration: 'none' }}>
+            <Card className='api-recipe' id={id} style={{ backgroundColor: '#fdbd10', color: '#ffffff', margin: '5px', border: '2px solid black', width: '300px', height: '300px' }}>
+                <div className='api-title'><Typography variant='h5' >{recipeName}</Typography></div>
+                <div className='api-img'><img src={imgSrc} /></div>
+            </Card>
         </Link>
     )
 }
